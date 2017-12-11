a89910c25b2c6b08e5cdf6f8030cafca41568a36
Beatrix Jones <beatrixdjones@gmail.com>
2017-12-10 19:57:22 -0500

    changed api calls

--- models/model.rb
+++ models/model.rb
@@ -3,10 +3,28 @@ require 'net/http'
 require 'json'
 require 'rubygems'
 
-# get the top 10 articles from IGN.com
-def ign_api_get_articles
-	#new_search_term = search_term.gsub(" ", "+")
-	request_string = "https://newsapi.org/v1/articles?source=ign&sortBy=top&apiKey=ecd08186d0394c99bcd808f2f76eed1b"
+# hash of news sources and their biases 
+# (based on information from mediabiasfactcheck.com)
+source_biases = {
+	"associated-press" => "least-biased",
+	"breitbart-news" => "strong-right",
+	"cnn" => "left",
+	"fox-news" => "right",
+	"msnbc" => "strong-left",
+	"the-new-york-times" => "left-center",
+	"the-wall-street-journal" => "right-center"
+}
+
+# get the top articles from news sources stored in hash
+def get_articles(source_hash)
+	# make string of all the sources in hash
+	sources_string = ""
+	source_hash.each_key do |source| 
+		sources_string += "#{source},"
+	end
+
+
+	request_string = "https://newsapi.org/v2/top-headlines?sources=#{sources_string}&apiKey=ecd08186d0394c99bcd808f2f76eed1b"
 	
 	sample_uri = URI(request_string) #opens a portal to the data at that link
 	sample_response = Net::HTTP.get(sample_uri) #go grab the data in the portal
@@ -14,81 +32,20 @@ def ign_api_get_articles
 	
 	# articles stored as an ARRAY stored in value of key "articles"
 	article_data = sample_parsedResponse["articles"]
-	# data is now an array of 10 articles
-	article_data
+	# data is now an array of articles from all sources
+	return article_data
 end
 
-#get the top 10 articles from Polygon.com
-def polygon_api_get_articles
-	#new_search_term = search_term.gsub(" ", "+")
-	request_string = "https://newsapi.org/v1/articles?source=polygon&sortBy=top&apiKey=ecd08186d0394c99bcd808f2f76eed1b"
-	
-	sample_uri = URI(request_string) #opens a portal to the data at that link
-	sample_response = Net::HTTP.get(sample_uri) #go grab the data in the portal
-	sample_parsedResponse = JSON.parse(sample_response) #makes data easy to read
-	
-	# articles stored as an ARRAY stored in value of key "articles"
-	article_data = sample_parsedResponse["articles"]
-	# data is now an array of 10 articles
-	article_data
-end
-
-#get the top 10 articles from TechRadar.com
-def techradar_api_get_articles
-	#new_search_term = search_term.gsub(" ", "+")
-	request_string = "https://newsapi.org/v1/articles?source=techradar&sortBy=top&apiKey=ecd08186d0394c99bcd808f2f76eed1b"
-	
-	sample_uri = URI(request_string) #opens a portal to the data at that link
-	sample_response = Net::HTTP.get(sample_uri) #go grab the data in the portal
-	sample_parsedResponse = JSON.parse(sample_response) #makes data easy to read
-	
-	# articles stored as an ARRAY stored in value of key "articles"
-	article_data = sample_parsedResponse["articles"]
-	# data is now an array of 10 articles
-	article_data
-end
-
-#get the top 10 articles from Mashable.com
-def mashable_api_get_articles
-	#new_search_term = search_term.gsub(" ", "+")
-	request_string = "https://newsapi.org/v1/articles?source=mashable&sortBy=top&apiKey=ecd08186d0394c99bcd808f2f76eed1b"
-	
-	sample_uri = URI(request_string) #opens a portal to the data at that link
-	sample_response = Net::HTTP.get(sample_uri) #go grab the data in the portal
-	sample_parsedResponse = JSON.parse(sample_response) #makes data easy to read
-	
-	# articles stored as an ARRAY stored in value of key "articles"
-	article_data = sample_parsedResponse["articles"]
-	# data is now an array of 10 articles
-	article_data
-end
-
-#get the top 10 articles from EntertainmentWeekly.com
-def ew_api_get_articles
-	#new_search_term = search_term.gsub(" ", "+")
-	request_string = "https://newsapi.org/v1/articles?source=entertainment-weekly&sortBy=top&apiKey=ecd08186d0394c99bcd808f2f76eed1b"
-	
-	sample_uri = URI(request_string) #opens a portal to the data at that link
-	sample_response = Net::HTTP.get(sample_uri) #go grab the data in the portal
-	sample_parsedResponse = JSON.parse(sample_response) #makes data easy to read
-	
-	# articles stored as an ARRAY stored in value of key "articles"
-	article_data = sample_parsedResponse["articles"]
-	# data is now an array of 10 articles
-	article_data
-end
+# p get_articles(source_biases)
 
 
-
-
-
-# method to get reviews
+# method to get headlines of articles
 # INPUT: an array of article hashes as fetched from an API
-# OUTPUT: an array of the urls of reviews
-def get_reviews(source_array)
+# OUTPUT: an array of the headlines of reviews
+def get_headlines(source_array)
     #search through urls for review
    
-   article_array=[]
+   headline_array=[]
    
    source_array.each do |article_hash|
       #get the url for each article
@@ -100,13 +57,8 @@ def get_reviews(source_array)
       end
    end
    #return the array of review article hashes
-   return article_array
+   return headline_array
 end
 
-#call above method that accesses IGN api
 
-    # p get_reviews(ign_api_get_articles)
-    # p get_reviews(polygon_api_get_articles)
-    # p get_reviews(techradar_api_get_articles)
-    # p get_reviews(mashable_api_get_articles)
-    # p get_reviews(ew_api_get_articles)
+
